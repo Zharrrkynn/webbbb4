c59595b719b12559174dbd54bbe6a0293bc8be4b
Zharrrkynn <mernibekz@icloud.com>
2026-02-04 17:44:00 +0500

    Remove public route comment from categoryRoutes
    Removed comment indicating public route.

--- routes/categoryRoutes.js
+++ routes/categoryRoutes.js
@@ -3,10 +3,7 @@ const router = express.Router();
 const categoryController = require('../controllers/categoryController');
 const { auth, adminCheck } = require('../middleware/auth');
 
-// Публичный маршрут
 router.get('/', categoryController.getAllCategories);
-
-// Защищенные маршруты для админа
 router.post('/', auth, adminCheck, categoryController.createCategory);
 router.put('/:id', auth, adminCheck, categoryController.updateCategory);
 router.delete('/:id', auth, adminCheck, categoryController.deleteCategory);
