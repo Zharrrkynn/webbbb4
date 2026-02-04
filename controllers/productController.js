a8b5079682d3a5aa3a3709558c8c09512fc45b7c
Zharrrkynn <mernibekz@icloud.com>
2026-02-04 17:39:12 +0500

    Add error handling to product controller methods

--- controllers/productController.js
+++ controllers/productController.js
@@ -1,22 +1,38 @@
 const Product = require('../models/product');
 
 exports.getAll = async (req, res) => {
-    const products = await Product.find().populate('category');
-    res.json(products);
+    try {
+        const products = await Product.find().populate('category');
+        res.json(products);
+    } catch (err) {
+        res.status(500).json({ error: err.message });
+    }
 };
 
 exports.create = async (req, res) => {
-    const product = new Product(req.body);
-    await product.save();
-    res.status(201).json(product);
+    try {
+        const product = new Product(req.body);
+        await product.save();
+        res.status(201).json(product);
+    } catch (err) {
+        res.status(400).json({ error: err.message });
+    }
 };
 
 exports.update = async (req, res) => {
-    const product = await Product.findByIdAndUpdate(req.params.id, req.body, { new: true });
-    res.json(product);
+    try {
+        const product = await Product.findByIdAndUpdate(req.params.id, req.body, { new: true });
+        res.json(product);
+    } catch (err) {
+        res.status(400).json({ error: err.message });
+    }
 };
 
 exports.delete = async (req, res) => {
-    await Product.findByIdAndDelete(req.params.id);
-    res.json({ message: 'Deleted' });
+    try {
+        await Product.findByIdAndDelete(req.params.id);
+        res.json({ message: 'Deleted' });
+    } catch (err) {
+        res.status(500).json({ error: err.message });
+    }
 };
