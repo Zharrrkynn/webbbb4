cd9279d7f9645f481671f81198d808c721e108e0
Zharrrkynn <mernibekz@icloud.com>
2026-02-04 17:40:47 +0500

    Simplify password hashing in user schema

--- models/User.js
+++ models/User.js
@@ -7,16 +7,10 @@ const userSchema = new mongoose.Schema({
     role: { type: String, enum: ['user', 'admin'], default: 'user' }
 });
 
-
 userSchema.pre('save', async function(next) {
     if (!this.isModified('password')) return next();
-    try {
-        const salt = await bcrypt.genSalt(10);
-        this.password = await bcrypt.hash(this.password, salt);
-        next(); 
-    } catch (error) {
-        next(error); 
-    }
+    this.password = await bcrypt.hash(this.password, 10);
+    next();
 });
 
 module.exports = mongoose.model('User', userSchema);
