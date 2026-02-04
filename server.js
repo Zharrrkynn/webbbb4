0e89ee1b28e5263b717870f689a7bb894a8beedb
Zharrrkynn <mernibekz@icloud.com>
2026-02-04 17:46:01 +0500

    Import path module in server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
-const path = require('path'); // Добавьте это
+const path = require('path'); 
 
 const app = express();
 app.use(express.json());
