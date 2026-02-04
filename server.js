933fc91a8290575580671f3b76e3225f375287c2
Zharrrkynn <mernibekz@icloud.com>
2026-02-04 17:45:36 +0500

    Add static file serving and update server log message

--- server.js
+++ server.js
@@ -1,11 +1,13 @@
 require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
+const path = require('path'); // Добавьте это
 
 const app = express();
 app.use(express.json());
 
-// Подключение маршрутов
+app.use(express.static(path.join(__dirname, 'public')));
+
 app.use('/api/auth', require('./routes/authRoutes'));
 app.use('/api/products', require('./routes/productRoutes'));
 app.use('/api/categories', require('./routes/categoryRoutes'));
@@ -15,4 +17,4 @@ mongoose.connect(process.env.MONGO_URI)
     .catch(err => console.error('❌ Connection error:', err));
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
+app.listen(PORT, () => console.log(`🚀 Server running on http://localhost:${PORT}`));
