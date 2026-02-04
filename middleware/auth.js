a86db224bc260e9efd304bf6abf9243d2ce8412b
Zharrrkynn <mernibekz@icloud.com>
2026-02-04 17:39:52 +0500

    Improve error messages in auth middleware

--- middleware/auth.js
+++ middleware/auth.js
@@ -2,14 +2,14 @@ const jwt = require('jsonwebtoken');
 
 exports.auth = (req, res, next) => {
     const token = req.header('Authorization')?.replace('Bearer ', '');
-    if (!token) return res.status(401).json({ message: 'No token, authorization denied' });
+    if (!token) return res.status(401).json({ message: 'Access denied. No token.' });
 
     try {
         const decoded = jwt.verify(token, process.env.JWT_SECRET);
         req.user = decoded;
         next();
-    } catch (e) {
-        res.status(401).json({ message: 'Token is not valid' });
+    } catch (ex) {
+        res.status(400).json({ message: 'Invalid token.' });
     }
 };
 
